ccbf82027e2c71b2f7661cd352501671bdecf8e5
kimhw1203 <kimhw1203@gmail.com>
2024-06-12 00:32:21 +0900

    Create results.js

---
+++ results.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const resultData = JSON.parse(localStorage.getItem('bingoResult'));
+    const resultDiv = document.getElementById('result');
+
+    if (resultData) {
+        resultDiv.innerHTML = `
+            <p>You got ${resultData.correctCount} correct answers.</p>
+            <p>You achieved ${resultData.bingoCount} bingo(s): ${resultData.grade}</p>
+        `;
+    } else {
+        resultDiv.innerHTML = '<p>No results to display.</p>';
+    }
+});
